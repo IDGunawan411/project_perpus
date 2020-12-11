3ce61357ba718c011e5afa5357ae61090fd8786e
IDGunawan411 <63904116+IDGunawan411@users.noreply.github.com>
2020-12-11 15:53:06 +0700

    commit all

---
+++ session/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+//variabel koneksi
+$koneksi = mysqli_connect("localhost","id14847421_gunawan02","p3rpus*gunawanP","id14847421_perpustakaan");
+
+if(!$koneksi){
+	echo "Koneksi Database Gagal...!!!";
+}
+?>
